9305b66fe4c989c1f37cb81a020a2178dd054d86
vrivoire <rivoire.vincent@gmail.com>
2024-08-27 03:39:54 -0400

    simplier

--- CreateEXEs.bat
+++ CreateEXEs.bat
@@ -1,13 +1,10 @@
 @echo 0ff
 cls
 
-set PoidsPression=%HOMEDRIVE%%HOMEPATH%\Documents\NetBeansProjects\PycharmProjects\PoidsPression
-rem cd %PoidsPression%
-
 @echo -------------------------------------------------
-pyinstaller --onefile Poids.py --icon=%PoidsPression%\weight-scale.ico --nowindowed --noconsole
+pyinstaller --onefile Poids.py --icon=weight-scale.ico --nowindowed --noconsole
 @echo -------------------------------------------------
-pyinstaller --onefile Pression3.py --icon=%PoidsPression%\pression.ico --nowindowed --noconsole
+pyinstaller --onefile Pression3.py --icon=pression.ico --nowindowed --noconsole
 @echo -------------------------------------------------
 
 pause
