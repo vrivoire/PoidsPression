a94db7c04574b6237cd5e88a6b0adf61bceccd20
vrivoire <rivoire.vincent@gmail.com>
2024-12-13 13:40:08 -0500

    better logging

--- Poids.py
+++ Poids.py
@@ -21,130 +21,142 @@ CSV_FILE = "Renpho Health R_PmJP0.csv"
 ZIP_FILE = "takeout-*.zip"
 DAYS = 180
 
+LOG_PATH = f"{PATH}logs/"
+LOG_FILE = f'{LOG_PATH}Poids.log'
+
+
+def namer(name: str) -> str:
+    return name.replace(".log", "") + ".log"
+
+
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
+fileHandler = logging.handlers.TimedRotatingFileHandler(LOG_FILE, when='midnight', interval=1, backupCount=7, encoding=None, delay=False, utc=False, atTime=None, errors=None)
+fileHandler.namer = namer
 log.basicConfig(
-	level=logging.INFO,
-	format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] [%(lineno)d] %(message)s",
-	handlers=[
-		logging.handlers.TimedRotatingFileHandler(PATH + "Poids.log", when='midnight', interval=1, backupCount=7, encoding=None, delay=False, utc=False, atTime=None, errors=None),
-		logging.StreamHandler()
-	]
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)-8s] [%(filename)s.%(funcName)s:%(lineno)d] %(message)s",
+    handlers=[
+        fileHandler,
+        logging.StreamHandler()
+    ]
 )
 
 
 def load_csv():
-	if os.path.isfile(PATH + CSV_FILE):
-		result = pd.read_csv(PATH + CSV_FILE)
-		result = result.rename(columns={'Données de temps': 'date', 'Poids(kg)': 'kg'})
-		result = result.drop(['IMC', 'Graisse corporelle(%)', 'Poids hors masse grasse(kg)', 'Gras sous-cutané(%)', 'Graisse viscérale', 'Eau Corporelle Totale(%)', 'Muscle squelettique(%)', 'Masse musculaire(kg)', 'Masse osseuse(kg)', 'Protéines(%)', 'Métabolisme de base(kcal)', 'Âge métabolique', 'Remarques'], axis=1)
-		result = result.astype({'date': 'datetime64[ns]'})
-		result = result.astype({'kg': 'float'})
-		return result.to_dict('records')
-	return []
+    if os.path.isfile(PATH + CSV_FILE):
+        result = pd.read_csv(PATH + CSV_FILE)
+        result = result.rename(columns={'Données de temps': 'date', 'Poids(kg)': 'kg'})
+        result = result.drop(['IMC', 'Graisse corporelle(%)', 'Poids hors masse grasse(kg)', 'Gras sous-cutané(%)', 'Graisse viscérale', 'Eau Corporelle Totale(%)', 'Muscle squelettique(%)', 'Masse musculaire(kg)', 'Masse osseuse(kg)', 'Protéines(%)', 'Métabolisme de base(kcal)', 'Âge métabolique', 'Remarques'], axis=1)
+        result = result.astype({'date': 'datetime64[ns]'})
+        result = result.astype({'kg': 'float'})
+        return result.to_dict('records')
+    return []
 
 
 def add_data(file_name):
-	data = []
-	result = pd.read_json(file_name)
-	for dataPoint in result["Data Points"]:
-		kg = float(dataPoint["fitValue"][0]["value"]["fpVal"])
-		date = pd.to_datetime(
-			int(dataPoint["endTimeNanos"]) / 1000000, utc=False, unit="ms"
-		)
-		if date.year >= 2021:
-			data.append({"kg": kg, "date": date})
-	return data
+    data = []
+    result = pd.read_json(file_name)
+    for dataPoint in result["Data Points"]:
+        kg = float(dataPoint["fitValue"][0]["value"]["fpVal"])
+        date = pd.to_datetime(
+            int(dataPoint["endTimeNanos"]) / 1000000, utc=False, unit="ms"
+        )
+        if date.year >= 2021:
+            data.append({"kg": kg, "date": date})
+    return data
 
 
 def show():
-	plt.plot(df["date"], df["kg"], color="g")
+    plt.plot(df["date"], df["kg"], color="g")
 
-	mean = df.rolling(window=f'{DAYS}D', on='date')['kg'].mean()
-	plt.plot(df["date"], mean, color='0.5')
+    mean = df.rolling(window=f'{DAYS}D', on='date')['kg'].mean()
+    plt.plot(df["date"], mean, color='0.5')
 
-	plt.axvline(datetime(2023, 2, 2))
+    plt.axvline(datetime(2023, 2, 2))
 
-	plt.tight_layout()
-	plt.grid(which="both")
-	plt.grid(which="major", linewidth=1)
-	plt.grid(which="minor", linewidth=0.2)
-	max_kg = df['kg'].max(numeric_only=True)
-	min_kg = df['kg'].min(numeric_only=True)
-	plt.title(f"Poids: {df['kg'][len(df['kg']) - 1]}, min: {round(min_kg, 2)}Kg, max: {round(max_kg, 2)}Kg, Δ: {round(max_kg - min_kg, 2)}Kg, x̄: {round(mean[mean.size - 1], 2)}Kg (rolling x̄: {DAYS} days)")
-	max_kg = int(max_kg) + 1
-	min_kg = int(min_kg) - 1
-	x_min = df['date'][0] - timedelta(days=10)
-	x_max = df["date"][df["date"].size - 1] + timedelta(days=10)
-	plt.axis((x_min, x_max, min_kg, max_kg))
-	yticks = []
-	count = min_kg
-	while count <= max_kg:
-		yticks.append(count)
-		count += 1
-	plt.yticks(yticks)
-	plt.minorticks_on()
+    plt.tight_layout()
+    plt.grid(which="both")
+    plt.grid(which="major", linewidth=1)
+    plt.grid(which="minor", linewidth=0.2)
+    max_kg = df['kg'].max(numeric_only=True)
+    min_kg = df['kg'].min(numeric_only=True)
+    plt.title(f"Poids: {df['kg'][len(df['kg']) - 1]}, min: {round(min_kg, 2)}Kg, max: {round(max_kg, 2)}Kg, Δ: {round(max_kg - min_kg, 2)}Kg, x̄: {round(mean[mean.size - 1], 2)}Kg (rolling x̄: {DAYS} days)")
+    max_kg = int(max_kg) + 1
+    min_kg = int(min_kg) - 1
+    x_min = df['date'][0] - timedelta(days=10)
+    x_max = df["date"][df["date"].size - 1] + timedelta(days=10)
+    plt.axis((x_min, x_max, min_kg, max_kg))
+    yticks = []
+    count = min_kg
+    while count <= max_kg:
+        yticks.append(count)
+        count += 1
+    plt.yticks(yticks)
+    plt.minorticks_on()
 
-	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
-	plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-	plt.xticks(rotation=45, ha='right', fontsize='small')
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
+    plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+    plt.xticks(rotation=45, ha='right', fontsize='small')
 
-	fig = plt.gcf()
-	fig.subplots_adjust(
-		left=0.055,
-		bottom=0.105,
-		right=0.952,
-		top=0.948,
-		wspace=0.198,
-		hspace=0.202
-	)
-	fig.canvas.manager.set_window_title('Poids')
-	DPI = fig.get_dpi()
-	fig.set_size_inches(1280.0 / float(DPI), 720.0 / float(DPI))
-	plt.savefig(PATH + 'Poids.png')
+    fig = plt.gcf()
+    fig.subplots_adjust(
+        left=0.055,
+        bottom=0.105,
+        right=0.952,
+        top=0.948,
+        wspace=0.198,
+        hspace=0.202
+    )
+    fig.canvas.manager.set_window_title('Poids')
+    DPI = fig.get_dpi()
+    fig.set_size_inches(1280.0 / float(DPI), 720.0 / float(DPI))
+    plt.savefig(PATH + 'Poids.png')
 
-	plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
-	zip_list = glob.glob(DL_PATH + ZIP_FILE)
-	if len(zip_list) > 0:
-		log.info("Found zipfile from Takeout: " + zip_list[0])
-		shutil.unpack_archive(zip_list[0], DL_PATH)
-		os.remove(zip_list[0])
+    zip_list = glob.glob(DL_PATH + ZIP_FILE)
+    if len(zip_list) > 0:
+        log.info("Found zipfile from Takeout: " + zip_list[0])
+        shutil.unpack_archive(zip_list[0], DL_PATH)
+        os.remove(zip_list[0])
 
-	if os.path.isfile(CLOUD_PATH + JSON_FILE):
-		shutil.copy2(CLOUD_PATH + JSON_FILE, PATH)
-		shutil.rmtree(CLOUD_PATH + '/../..')
+    if os.path.isfile(CLOUD_PATH + JSON_FILE):
+        shutil.copy2(CLOUD_PATH + JSON_FILE, PATH)
+        shutil.rmtree(CLOUD_PATH + '/../..')
 
-	cloud_data = []
-	if len(cloud_data) == 0:
-		cloud_data = add_data(PATH + JSON_FILE)
+    cloud_data = []
+    if len(cloud_data) == 0:
+        cloud_data = add_data(PATH + JSON_FILE)
 
-	csv_file = PATH + CSV_FILE
-	log.info(f"csv_file = {csv_file}")
-	csv_root = csv_file[0: (len(csv_file) - 4)]
-	log.info(f"csv_root = {csv_root}")
+    csv_file = PATH + CSV_FILE
+    log.info(f"csv_file = {csv_file}")
+    csv_root = csv_file[0: (len(csv_file) - 4)]
+    log.info(f"csv_root = {csv_root}")
 
-	if os.path.isfile(csv_root):
-		log.info("Found csv_root")
-		if os.path.isfile(csv_file):
-			os.remove(csv_root + ".csv")
-		os.rename(csv_root, csv_file)
+    if os.path.isfile(csv_root):
+        log.info("Found csv_root")
+        if os.path.isfile(csv_file):
+            os.remove(csv_root + ".csv")
+        os.rename(csv_root, csv_file)
 
-	if os.path.isfile(DL_PATH + CSV_FILE):
-		shutil.copy2(DL_PATH + CSV_FILE, PATH)
-		os.remove(DL_PATH + CSV_FILE)
-	csv_data = load_csv()
+    if os.path.isfile(DL_PATH + CSV_FILE):
+        shutil.copy2(DL_PATH + CSV_FILE, PATH)
+        os.remove(DL_PATH + CSV_FILE)
+    csv_data = load_csv()
 
-	results = []
-	cloud_data.extend(csv_data)
-	for myDict in cloud_data:
-		if myDict not in results:
-			results.append(myDict)
-		else:
-			log.info(myDict)
+    results = []
+    cloud_data.extend(csv_data)
+    for myDict in cloud_data:
+        if myDict not in results:
+            results.append(myDict)
+        else:
+            log.info(myDict)
 
-	sortedDatas = sorted(results, key=lambda d: d["date"])
-	df = pd.DataFrame(sortedDatas)
-	df.to_csv(PATH + 'poids.csv', encoding='utf-8', index=False, date_format="%Y-%m-%dT%H:%M:%S")
-	log.info('\n' + df.to_string())
-	show()
+    sortedDatas = sorted(results, key=lambda d: d["date"])
+    df = pd.DataFrame(sortedDatas)
+    df.to_csv(PATH + 'poids.csv', encoding='utf-8', index=False, date_format="%Y-%m-%dT%H:%M:%S")
+    log.info('\n' + df.to_string())
+    show()
--- Pression.py
+++ Pression.py
@@ -7,6 +7,7 @@ import traceback
 from datetime import datetime, timedelta, date
 from tkinter import Entry, IntVar, Tk
 from typing import Any
+import os
 
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
@@ -16,193 +17,206 @@ VERSION = 3
 PATH = "G:/My Drive/PoidsPression/"
 DAYS = 60
 
+
+LOG_PATH = f"{PATH}logs/"
+LOG_FILE = f'{LOG_PATH}Pression.log'
+
+
+def namer(name: str) -> str:
+    return name.replace(".log", "") + ".log"
+
+
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
+fileHandler = logging.handlers.TimedRotatingFileHandler(LOG_FILE, when='midnight', interval=1, backupCount=7, encoding=None, delay=False, utc=False, atTime=None, errors=None)
+fileHandler.namer = namer
 log.basicConfig(
-	level=logging.INFO,
-	format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] [%(lineno)d] %(message)s",
-	handlers=[
-		logging.handlers.TimedRotatingFileHandler(f'{PATH}Pression.log', when='midnight', interval=1, backupCount=7, encoding=None, delay=False, utc=False, atTime=None, errors=None),
-		logging.StreamHandler()
-	]
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)-8s] [%(filename)s.%(funcName)s:%(lineno)d] %(message)s",
+    handlers=[
+        fileHandler,
+        logging.StreamHandler()
+    ]
 )
 
 
 class Pression:
 
-	def __init__(self):
-		log.info('Starting')
+    def __init__(self):
+        log.info('Starting')
 
-	@staticmethod
-	def load_csv() -> list[dict]:
-		try:
-			df: pd.DataFrame = pd.read_csv(f'{PATH}pression.csv')
-			df['date'] = pd.to_datetime(df['date'])
-			return df.to_dict('records')
-		except (FileNotFoundError, pd.errors.EmptyDataError) as ex:
-			log.error(ex)
-			pressure_list: list[dict[str, datetime]] = []
-			return pressure_list
+    @staticmethod
+    def load_csv() -> list[dict]:
+        try:
+            df: pd.DataFrame = pd.read_csv(f'{PATH}pression.csv')
+            df['date'] = pd.to_datetime(df['date'])
+            return df.to_dict('records')
+        except (FileNotFoundError, pd.errors.EmptyDataError) as ex:
+            log.error(ex)
+            pressure_list: list[dict[str, datetime]] = []
+            return pressure_list
 
-	@staticmethod
-	def save_csv(pressure_list: list[dict[str, datetime]]) -> None:
-		df: pd.DataFrame = pd.DataFrame(pressure_list)
-		df.to_csv(f'{PATH}pression.csv', encoding='utf-8', index=False, date_format="%Y-%m-%dT%H:%M:%S")
+    @staticmethod
+    def save_csv(pressure_list: list[dict[str, datetime]]) -> None:
+        df: pd.DataFrame = pd.DataFrame(pressure_list)
+        df.to_csv(f'{PATH}pression.csv', encoding='utf-8', index=False, date_format="%Y-%m-%dT%H:%M:%S")
 
-	@staticmethod
-	def display(pressure_list: list[dict[str, datetime]]) -> None:
-		df: pd.DataFrame = pd.DataFrame(pressure_list)
+    @staticmethod
+    def display(pressure_list: list[dict[str, datetime]]) -> None:
+        df: pd.DataFrame = pd.DataFrame(pressure_list)
 
-		mean = df.rolling(window=f'{DAYS}D', on='date')['sys'].mean()
-		plt.plot(df["date"], mean, color='darkgreen')
-		mean = df.rolling(window=f'{DAYS}D', on='date')['dia'].mean()
-		plt.plot(df["date"], mean, color='darkblue')
-		# mean = df["pulse"].rolling(window=WINDOW).mean()
-		# plt.plot(df["date"], mean, color='darkred')
+        mean = df.rolling(window=f'{DAYS}D', on='date')['sys'].mean()
+        plt.plot(df["date"], mean, color='darkgreen')
+        mean = df.rolling(window=f'{DAYS}D', on='date')['dia'].mean()
+        plt.plot(df["date"], mean, color='darkblue')
+        # mean = df["pulse"].rolling(window=WINDOW).mean()
+        # plt.plot(df["date"], mean, color='darkred')
 
-		plt.plot(df["date"], df["sys"], "g", label=f'Sys')
-		plt.plot(df["date"], df["dia"], "b", label=f'Dia')
+        plt.plot(df["date"], df["sys"], "g", label=f'Sys')
+        plt.plot(df["date"], df["dia"], "b", label=f'Dia')
 
-		max_sys: int = int(df['sys'].max(numeric_only=True)) + 1
-		min_sys: int = int(df['sys'].min(numeric_only=True)) - 1
-		log.info(f"max_sys: {max_sys}, min_sys: {min_sys}")
-		max_dia: int = int(df['dia'].max(numeric_only=True)) + 1
-		min_dia: int = int(df['dia'].min(numeric_only=True)) - 1
-		log.info(f"max_dia: {max_dia}, min_dia: {min_dia}")
+        max_sys: int = int(df['sys'].max(numeric_only=True)) + 1
+        min_sys: int = int(df['sys'].min(numeric_only=True)) - 1
+        log.info(f"max_sys: {max_sys}, min_sys: {min_sys}")
+        max_dia: int = int(df['dia'].max(numeric_only=True)) + 1
+        min_dia: int = int(df['dia'].min(numeric_only=True)) - 1
+        log.info(f"max_dia: {max_dia}, min_dia: {min_dia}")
 
-		maximum: int = max_sys if max_sys > max_dia else max_dia
-		minimum: int = min_sys if min_sys < min_dia else min_dia
+        maximum: int = max_sys if max_sys > max_dia else max_dia
+        minimum: int = min_sys if min_sys < min_dia else min_dia
 
-		plt.axis((df["date"][0], df["date"][df["date"].size - 1], minimum, maximum))
+        plt.axis((df["date"][0], df["date"][df["date"].size - 1], minimum, maximum))
 
-		yticks: list[int] = []
-		count: int = minimum
-		while count <= maximum:
-			yticks.append(count)
-			count += 5
-		plt.yticks(yticks)
-		plt.minorticks_on()
+        yticks: list[int] = []
+        count: int = minimum
+        while count <= maximum:
+            yticks.append(count)
+            count += 5
+        plt.yticks(yticks)
+        plt.minorticks_on()
 
-		if df['sys'].max() >= 160:
-			plt.axhspan(160, 165, color='xkcd:pastel orange', linestyle='-')
-		plt.axhspan(140, 160, color='xkcd:light orange', linestyle='-')
-		plt.axhspan(120, 140, color='xkcd:apricot', linestyle='-')
+        if df['sys'].max() >= 160:
+            plt.axhspan(160, 165, color='xkcd:pastel orange', linestyle='-')
+        plt.axhspan(140, 160, color='xkcd:light orange', linestyle='-')
+        plt.axhspan(120, 140, color='xkcd:apricot', linestyle='-')
 
-		plt.axhspan(100, 110, color='xkcd:pastel orange', linestyle='-')
-		plt.axhspan(90, 100, color='xkcd:light orange', linestyle='-')
-		plt.axhspan(80, 90, color='xkcd:apricot', linestyle='-')
+        plt.axhspan(100, 110, color='xkcd:pastel orange', linestyle='-')
+        plt.axhspan(90, 100, color='xkcd:light orange', linestyle='-')
+        plt.axhspan(80, 90, color='xkcd:apricot', linestyle='-')
 
-		plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
-		plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-		plt.gca().xaxis.set_minor_locator(mdates.DayLocator(interval=7))
-		plt.xticks(rotation=45, ha='right', fontsize='small')
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m'))
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+        plt.gca().xaxis.set_minor_locator(mdates.DayLocator(interval=7))
+        plt.xticks(rotation=45, ha='right', fontsize='small')
 
-		plt.legend()
-		plt.tight_layout()
-		plt.grid(which="both")
-		plt.grid(which="major", linewidth=1)
-		plt.grid(which="minor", linewidth=0.2)
-		plt.minorticks_on()
+        plt.legend()
+        plt.tight_layout()
+        plt.grid(which="both")
+        plt.grid(which="major", linewidth=1)
+        plt.grid(which="minor", linewidth=0.2)
+        plt.minorticks_on()
 
-		ax: plt.axes.Axes = plt.gca()
-		x_min: datetime = pressure_list[0]['date'] - timedelta(days=10)
-		x_max: datetime = pressure_list[len(pressure_list) - 1]['date'] + timedelta(days=10)
-		ax.set_xlim(x_min, x_max)
+        ax: plt.axes.Axes = plt.gca()
+        x_min: datetime = pressure_list[0]['date'] - timedelta(days=10)
+        x_max: datetime = pressure_list[len(pressure_list) - 1]['date'] + timedelta(days=10)
+        ax.set_xlim(x_min, x_max)
 
-		fig: plt.axes.Figure = plt.gcf()
-		fig.subplots_adjust(
-			left=0.055,
-			bottom=0.105,
-			right=0.952,
-			top=0.948,
-			wspace=0.198,
-			hspace=0.202
-		)
-		fig.canvas.manager.set_window_title(f'Pression {VERSION}')
-		DPI: float = fig.get_dpi()
-		fig.set_size_inches(1280.0 / float(DPI), 720.0 / float(DPI))
-		print()
-		plt.title(f'Pression {VERSION} (x̄: {DAYS} days), Sys: {df['sys'][len(df['sys']) - 1]}, Dia: {df['dia'][len(df['dia']) - 1]}, Pulse: {df['pulse'][len(df['pulse']) - 1]}, Date: {df['date'][len(df['date']) - 1]}')
-		plt.savefig(PATH + 'pression.png')
-		plt.show()
+        fig: plt.axes.Figure = plt.gcf()
+        fig.subplots_adjust(
+            left=0.055,
+            bottom=0.105,
+            right=0.952,
+            top=0.948,
+            wspace=0.198,
+            hspace=0.202
+        )
+        fig.canvas.manager.set_window_title(f'Pression {VERSION}')
+        DPI: float = fig.get_dpi()
+        fig.set_size_inches(1280.0 / float(DPI), 720.0 / float(DPI))
+        print()
+        plt.title(f'Pression {VERSION} (x̄: {DAYS} days), Sys: {df['sys'][len(df['sys']) - 1]}, Dia: {df['dia'][len(df['dia']) - 1]}, Pulse: {df['pulse'][len(df['pulse']) - 1]}, Date: {df['date'][len(df['date']) - 1]}')
+        plt.savefig(PATH + 'pression.png')
+        plt.show()
 
-	@staticmethod
-	def add_line(line: dict) -> None:
-		pressure_list.append(line)
-		log.info(line)
+    @staticmethod
+    def add_line(line: dict) -> None:
+        pressure_list.append(line)
+        log.info(line)
 
 
 class Dialog:
-	ROOT: Tk = tk.Tk()
+    ROOT: Tk = tk.Tk()
 
-	def __init__(self, pression: Pression) -> None:
-		Dialog.ROOT.eval('tk::PlaceWindow . center')
-		Dialog.ROOT.title(f"Pression {VERSION}")
-		Dialog.ROOT.resizable(False, False)
+    def __init__(self, pression: Pression) -> None:
+        Dialog.ROOT.eval('tk::PlaceWindow . center')
+        Dialog.ROOT.title(f"Pression {VERSION}")
+        Dialog.ROOT.resizable(False, False)
 
-		row = tk.Frame(Dialog.ROOT)
-		tk.Label(row, width=20, text=f"Pression v{VERSION}", anchor='center', font=('calibre', 12, 'bold')).pack(side=tk.LEFT)
-		row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        row = tk.Frame(Dialog.ROOT)
+        tk.Label(row, width=20, text=f"Pression v{VERSION}", anchor='center', font=('calibre', 12, 'bold')).pack(side=tk.LEFT)
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
 
-		entries: list[tuple[Any, Entry, IntVar]] = self.make_form()
+        entries: list[tuple[Any, Entry, IntVar]] = self.make_form()
 
-		Dialog.ROOT.bind('<Return>', lambda event: self.submit(entries))
-		tk.Button(text='Show', width=20, command=lambda: self.submit(entries)).pack(padx=5, pady=5)
+        Dialog.ROOT.bind('<Return>', lambda event: self.submit(entries))
+        tk.Button(text='Show', width=20, command=lambda: self.submit(entries)).pack(padx=5, pady=5)
 
-		entries[0][1].focus()
-		Dialog.ROOT.mainloop()
+        entries[0][1].focus()
+        Dialog.ROOT.mainloop()
 
-	@staticmethod
-	def validate(character: Any, entry_value: str) -> object:
-		return True if (character in '0123456789 ' and len(entry_value) < 4) else False
+    @staticmethod
+    def validate(character: Any, entry_value: str) -> object:
+        return True if (character in '0123456789 ' and len(entry_value) < 4) else False
 
-	def make_form(self) -> list[tuple[Any, Entry, IntVar]]:
-		entries: list[tuple[Any, Entry, IntVar]] = []
-		for field in 'Sys', 'Dia', 'Pulse':
-			row = tk.Frame(Dialog.ROOT)
-			tk.Label(row, width=6, text=field, anchor='w', font=('calibre', 12, 'bold')).pack(side=tk.LEFT)
-			intVar = tk.IntVar(value='')
-			ent = tk.Entry(row, textvariable=intVar, validate='key', validatecommand=(Dialog.ROOT.register(self.validate), '%S', '%P'), font=('calibre', 12, 'normal'), width=4)
-			row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
-			ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
-			entries.append((field, ent, intVar))
-		return entries
+    def make_form(self) -> list[tuple[Any, Entry, IntVar]]:
+        entries: list[tuple[Any, Entry, IntVar]] = []
+        for field in 'Sys', 'Dia', 'Pulse':
+            row = tk.Frame(Dialog.ROOT)
+            tk.Label(row, width=6, text=field, anchor='w', font=('calibre', 12, 'bold')).pack(side=tk.LEFT)
+            intVar = tk.IntVar(value='')
+            ent = tk.Entry(row, textvariable=intVar, validate='key', validatecommand=(Dialog.ROOT.register(self.validate), '%S', '%P'), font=('calibre', 12, 'normal'), width=4)
+            row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+            ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+            entries.append((field, ent, intVar))
+        return entries
 
-	@staticmethod
-	def submit(entries: list[tuple[Any, Entry, IntVar]]) -> None:
-		error: bool = False
-		line: dict = {}
-		for entry in entries:
-			field = entry[0]
-			value: int
-			try:
-				value = entry[2].get()
-				line[field.lower()] = value
-			except tk.TclError as ex1:
-				error = True
-				log.error(f'{field}: {ex1}')
-		if not error:
-			line['date'] = datetime.now()
-			pression.add_line(line)
-			Dialog.ROOT.destroy()
-		elif entries[0][1].get() == '' and entries[1][1].get() == '' and entries[2][1].get() == '':
-			Dialog.ROOT.destroy()
+    @staticmethod
+    def submit(entries: list[tuple[Any, Entry, IntVar]]) -> None:
+        error: bool = False
+        line: dict = {}
+        for entry in entries:
+            field = entry[0]
+            value: int
+            try:
+                value = entry[2].get()
+                line[field.lower()] = value
+            except tk.TclError as ex1:
+                error = True
+                log.error(f'{field}: {ex1}')
+        if not error:
+            line['date'] = datetime.now()
+            pression.add_line(line)
+            Dialog.ROOT.destroy()
+        elif entries[0][1].get() == '' and entries[1][1].get() == '' and entries[2][1].get() == '':
+            Dialog.ROOT.destroy()
 
 
 if __name__ == "__main__":
-	try:
-		pression: Pression = Pression()
+    try:
+        pression: Pression = Pression()
 
-		start_time: float = time.time()
-		pressure_list: list[dict]
-		log.info(f'sys.argv: {sys.argv}')
+        start_time: float = time.time()
+        pressure_list: list[dict]
+        log.info(f'sys.argv: {sys.argv}')
 
-		pressure_list = pression.load_csv()
-		dialog: Dialog = Dialog(pression)
+        pressure_list = pression.load_csv()
+        dialog: Dialog = Dialog(pression)
 
-		log.info(f"\n{pd.DataFrame(pressure_list).to_string()}")
-		pression.save_csv(pressure_list)
-		pression.display(pressure_list)
-		log.info("--- %s seconds ---" % (time.time() - start_time))
-	# c = 5/ 0
-	except Exception as ex:
-		log.error(ex)
-		log.error(traceback.format_exc())
+        log.info(f"\n{pd.DataFrame(pressure_list).to_string()}")
+        pression.save_csv(pressure_list)
+        pression.display(pressure_list)
+        log.info("--- %s seconds ---" % (time.time() - start_time))
+    # c = 5/ 0
+    except Exception as ex:
+        log.error(ex)
+        log.error(traceback.format_exc())
