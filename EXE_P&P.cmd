fcc5b7ee4823224a44198de868c4c71e54d0e4bc
vrivoire <rivoire.vincent@gmail.com>
2025-07-24 12:26:46 -0400

    fix

--- EXE_P&P.cmd
+++ EXE_P&P.cmd
@@ -3,10 +3,10 @@ cls
 cd C:\Users\adele\Documents\NetBeansProjects\PycharmProjects\PoidsPression
 @echo -------------------------------------------------
 rem pyinstaller --onefile Poids.py --icon=weight-scale.ico --nowindowed --noconsole
-start pyinstaller --onedir  Poids.py --icon=poids.ico --nowindowed --noconsole --paths=C:\Users\ADELE\Documents\NetBeansProjects\PycharmProjects\PoidsPression
+start pyinstaller -y --onedir  Poids.py --icon=poids.ico --nowindowed --noconsole --paths=C:\Users\ADELE\Documents\NetBeansProjects\PycharmProjects\PoidsPression
 @echo -------------------------------------------------
 rem pyinstaller --onefile Pression.py --icon=pression.ico --nowindowed --noconsole
-start pyinstaller --onedir  Pression.py --icon=pression.png --nowindowed --noconsole --paths=C:\Users\ADELE\Documents\NetBeansProjects\PycharmProjects\PoidsPression
+start pyinstaller -y --onedir  Pression.py --icon=pression.png --nowindowed --noconsole --paths=C:\Users\ADELE\Documents\NetBeansProjects\PycharmProjects\PoidsPression
 @echo -------------------------------------------------
 
 rem 
