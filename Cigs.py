670ec625a5ec8df802e11a71b35b279d2ee54311
vrivoire <rivoire.vincent@gmail.com>
2025-07-27 11:22:00 -0400

    added

--- Cigs.py
+++ Cigs.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 
 
-def get_date_diff(start: str, end: str) -> None:
+def get_date_diff(start: str, end: str) -> str:
     d1 = datetime.strptime(start, "%d/%m/%Y %H:%M")
     d2 = datetime.strptime(end, "%d/%m/%Y %H:%M")
-    print( f'{start} - {end} = {abs(d1 - d2)}')
+    return f'{start} - {end} = {abs(d1 - d2)}'
 
 
-get_date_diff('25/07/2025 08:00', datetime.now().strftime("%d/%m/%Y %H:%M"))
+line: str = get_date_diff('25/07/2025 08:00', datetime.now().strftime("%d/%m/%Y %H:%M"))
+print(line)
 
+with open(__file__, "a", newline='') as file:
+    file.write('# '+ line + "\n")
+
+
+# History
