e66037e03b99c760721767c33c1613730857a100
vrivoire <rivoire.vincent@gmail.com>
2025-07-28 16:15:15 -0400

    Cleanup

--- Cigs.py
+++ Cigs.py
@@ -2,10 +2,10 @@ from datetime import datetime
 import subprocess
 
 
-def get_date_diff(start: str, end: str) -> str:
+def get_date_diff(start: str, end: str) -> (str, int):
     d1 = datetime.strptime(start, '%d/%m/%Y %H:%M')
     d2 = datetime.strptime(end, '%d/%m/%Y %H:%M')
-    return f'{start} - {end} = {abs(d1 - d2)}'
+    return f'{start} - {end} = {abs(d1 - d2)}', int(abs(d1 - d2).seconds)
 
 
 cig_file_name = 'C:/Users/ADELE/Documents/NetBeansProjects/PycharmProjects/PoidsPression/Cigs.py'
@@ -16,28 +16,25 @@ while len(last_line) == 0:
     with open(cig_file_name, 'r') as file:
         last_line = file.readlines()[i].strip()
 
-# print(f'last_line = {last_line}')
-
 last_timestamp = last_line[last_line.index('-') + 1:last_line.index('=')].strip()
-# print(f'last_timestamp = {last_timestamp}')
 
-line: str = get_date_diff(last_timestamp, datetime.now().strftime('%d/%m/%Y %H:%M'))
-# print(line)
+line: (str, int) = get_date_diff(last_timestamp, datetime.now().strftime('%d/%m/%Y %H:%M'))
 
-with open(cig_file_name, "a", newline='') as file:
-    file.write('# ' + line + "\n")
+string = f'{line[0]}\t{int((200.0 / (line[1] / 60 / 24)))} cig/j'
+print(string)
 
-# print(cig_file_name)
-# print(f'"C:/Program Files/Notepad++/notepad++.exe" "{cig_file_name}"')
+if not cig_file_name == __file__.replace('\\', '/'):
+    with open(cig_file_name, "a") as file:
+        file.write(f'# {string}\n')
 
-completed_process = subprocess.run(
-    f'"C:/Program Files/Notepad++/notepad++.exe" "{cig_file_name}"',
-    capture_output=True,
-    timeout=70,
-    encoding="utf-8",
-    check=False,
-    shell=True
-)
+    completed_process = subprocess.run(
+        f'"C:/Program Files/Notepad++/notepad++.exe" "{cig_file_name}"',
+        capture_output=True,
+        timeout=70,
+        encoding="utf-8",
+        check=False,
+        shell=True
+    )
 
 # History
 # 25/07/2025 08:00 - 27/07/2025 11:49 = 2 days, 3:49:00
