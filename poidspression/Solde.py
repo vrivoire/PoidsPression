06074ba50fdb853aee183cc681619557dd7b2347
vrivoire <rivoire.vincent@gmail.com>
2026-04-30 15:40:09 -0400

    New Graph

--- poidspression/Solde.py
+++ poidspression/Solde.py
@@ -1,11 +1,19 @@
 import os
+import tkinter
 import traceback
 import zipfile
 from pathlib import Path
 
-import pandas as pd
+import matplotlib
 from pandas import DataFrame
 
+matplotlib.use('TkAgg')
+import matplotlib.dates as m_dates
+import matplotlib.pyplot as plt
+import mplcursors
+import pandas as pd
+from matplotlib.dates import date2num, num2date
+from matplotlib.widgets import Slider, Button
 import poidspression
 from poidspression import log
 
@@ -27,14 +35,135 @@ COLS: dict[str, str] = {
     'Solde': 'Float64'
 }
 
+DAYS = 30.437 * 3
+
 
 class Solde:
     def __init__(self):
-        pass
+        df: DataFrame = self.prepare_data()
+        self.display_graph(df)
+        print('------------------------------------')
+        df.info()
+        print('------------------------------------')
+        # print(df.describe(include='all'))
+        # print('------------------------------------')
+        # print(df.shape)
 
-    def setup_columns(self, df: DataFrame):
-        df.astype(COLS)
-        df.sort_values(by=list(COLS.keys()), ascending=True, inplace=True)
+    def display_graph(self, df: DataFrame):
+        try:
+            fig, ax1 = plt.subplots()
+
+            dollars_graph, = ax1.plot(df["Date"], df["Solde"], color="g", label='$')
+            mean_dollars = df.rolling(window=f'{int(DAYS)}D', on='Date')['Solde'].mean()
+            mean_dollars_graph, = ax1.plot(df["Date"], df.rolling(center=True, window=f'{DAYS}D', on='Date')['Solde'].mean(), color='0.5', label='Mean $')
+
+            mplcursors.cursor(dollars_graph, hover=2).connect("add", lambda sel: sel.annotation.set_text(
+                f'{m_dates.num2date(sel.target[0]).strftime('%d/%m/%Y')}:  {round(float(sel[1][1]), 2)} {sel[0].get_label()}'
+            ))
+            mplcursors.cursor(mean_dollars_graph, hover=2).connect("add", lambda sel: sel.annotation.set_text(
+                f'{m_dates.num2date(sel.target[0]).strftime('%d/%m/%Y')}:  {round(float(sel[1][1]), 2)} {sel[0].get_label()}'
+            ))
+
+            plt.tight_layout()
+            plt.grid(which="both")
+            plt.grid(which="major", linewidth=1)
+            plt.grid(which="minor", linewidth=0.2)
+
+            title = plt.title(
+                f"Date: {df["Date"].tail(1).item().strftime('%d/%m/%Y')}, ${df["Solde"].tail(1).item():,.2f}, min: ${df['Solde'].min(numeric_only=True):.2f}, max: ${df['Solde'].max(numeric_only=True):,.2f}, x̄: ${mean_dollars.tail(1).item():,.2f}, rolling: {int(DAYS)} days)"
+            )
+
+            yticks: list[int] = []
+            count: int = df['Solde'].min(numeric_only=True)
+            while count <= df['Solde'].max(numeric_only=True):
+                yticks.append(count)
+                count += 10000
+
+            plt.minorticks_on()
+
+            plt.gca().xaxis.set_major_formatter(m_dates.DateFormatter('%m/%Y'))
+            plt.gca().xaxis.set_major_locator(m_dates.MonthLocator(interval=1))
+            plt.xticks(rotation=45, ha='right', fontsize='small')
+
+            plt.gcf()
+            fig.subplots_adjust(
+                left=0.055,
+                bottom=0.133,
+                right=0.952,
+                top=0.948,
+                wspace=0.198,
+                hspace=0.102
+            )
+            fig.canvas.manager.set_window_title('Solde')
+
+            def callback_update(val):
+                slider_position.valtext.set_text(num2date(val).date())
+                df2 = df[df['Date'].dt.date.between(num2date(val - DAYS).date(), num2date(val).date())]
+                ax1.axis((
+                    val - DAYS,
+                    val + 1,
+                    df2['Solde'].min(numeric_only=True) - 500,
+                    df2['Solde'].max(numeric_only=True) + 500
+                ))
+                mean_dollars = df2.rolling(window=f'{int(DAYS)}D', on='Date')['Solde'].mean()
+                title.set_text(
+                    f"Date: {df2["Date"].tail(1).item().strftime('%d/%m/%Y')}, ${df2["Solde"].tail(1).item():,.2f}, min: ${df2['Solde'].min(numeric_only=True):,.2f}, max: ${df2['Solde'].max(numeric_only=True):,.2f}, x̄: ${mean_dollars.tail(1).item():,.2f}, rolling: {int(DAYS)} days)"
+                )
+                fig.canvas.draw_idle()
+
+            def callback_reset(event):
+                slider_position.reset()
+                ax1.axis((
+                    date2num(df["Date"][0]),
+                    date2num(df['Date'][len(df['Date']) - 1]),
+                    df['Solde'].min(numeric_only=True) - 500,
+                    df['Solde'].max(numeric_only=True) + 500
+                ))
+                mean_dollars = df.rolling(window=f'{int(DAYS)}D', on='Date')['Solde'].mean()
+                title.set_text(
+                    f"Date: {df["Date"].tail(1).item().strftime('%d/%m/%Y')}, ${df["Solde"].tail(1).item():,.2f}, min: ${df['Solde'].min(numeric_only=True):,.2f}, max: ${df['Solde'].max(numeric_only=True):,.2f}, x̄: ${mean_dollars.tail(1).item():,.2f}, rolling: {int(DAYS)} days)"
+                )
+                fig.canvas.draw_idle()
+
+            slider_position = Slider(
+                plt.axes((0.08, 0.01, 0.73, 0.03), facecolor='White'),
+                'Date',
+                date2num(df["Date"][0]),
+                date2num(df['Date'].tail(1).item()),
+                valstep=1,
+                color='w',
+                initcolor='none'
+            )
+            slider_position.valtext.set_text(df["Date"][0].date())
+            slider_position.on_changed(callback_update)
+            button = Button(fig.add_axes((0.9, 0.01, 0.055, 0.03)), 'Reset', hovercolor='0.975')
+            button.on_clicked(callback_reset)
+
+            slider_position.set_val(date2num(df['Date'][len(df['Date']) - 1]))
+
+            mng = plt.get_current_fig_manager()
+            mng.window.state('zoomed')
+
+            dpi: float = fig.get_dpi()
+            root = tkinter.Tk()
+            screen_width: int = root.winfo_screenwidth()
+            screen_height: int = root.winfo_screenheight()
+            root.destroy()
+            fig.set_size_inches(screen_width / float(dpi), screen_height / float(dpi))
+            # plt.savefig(LOCAL_PATH + 'Poids.png')
+
+            plt.show()
+        except Exception as ex:
+            log.error(ex)
+            log.error(traceback.format_exc())
+
+    def setup_columns(self, df: DataFrame) -> DataFrame:
+        df = df.astype(COLS)
+        df = df.astype({'Date': 'datetime64[ns]'})
+        df = df.sort_values(by=list(COLS.keys()), ascending=True)
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.reset_index(drop=True, inplace=True)
+        return df
 
     def load_csv(self, file_name: str = BKP_FULL_PATH) -> DataFrame:
         try:
@@ -42,7 +171,7 @@ class Solde:
                 load_csv_df = DataFrame(pd.read_csv(file_name, header=0))
                 if load_csv_df is not None:
                     load_csv_df = load_csv_df[list(COLS.keys())]
-                    self.setup_columns(load_csv_df)
+                    load_csv_df = self.setup_columns(load_csv_df)
                     poidspression.show_df(load_csv_df, title=file_name, max_rows=10)
                     log.info(f'Loaded file: {file_name}, with {len(load_csv_df)} rows')
                     return load_csv_df
@@ -52,7 +181,7 @@ class Solde:
                 log.warning(f'Could not find file: {file_name}')
 
             load_csv_df = pd.DataFrame(columns=list(COLS.keys()))
-            self.setup_columns(load_csv_df)
+            load_csv_df = self.setup_columns(load_csv_df)
             return load_csv_df
         except Exception as ex:
             log.error(ex)
@@ -76,16 +205,11 @@ class Solde:
 
     def drop_duplicates(self, df):
         try:
-            df.reset_index(inplace=True)
             len1: int = len(df)
             diff_df = df[df.duplicated(keep='first')]
             log.info(f"Removed Rows: {len(diff_df)}")
-
             df.drop_duplicates(subset=list(COLS.keys()), keep='first', inplace=True)
-
             log.info(f'drop_duplicates: removed {len1 - len(df)} rows from {len1}')
-            df.set_index('Date')
-            df.reindex(columns=['Date'])
         except Exception as ex:
             log.error(ex)
             log.error(traceback.format_exc())
@@ -100,10 +224,8 @@ class Solde:
                     df = pd.concat([df_tmp, df], axis=0, ignore_index=True, join='outer')
                     log.info(f'\t\tConcated: {path}, now {len(df)} rows')
 
-                self.setup_columns(df)
-                df = df.set_index('Date')
-
                 self.drop_duplicates(df)
+                df = self.setup_columns(df)
 
                 poidspression.show_df(df, title='Result *********', max_rows=10)
 
@@ -133,14 +255,8 @@ class Solde:
 if __name__ == "__main__":
     try:
         poidspression.set_up(__file__)
-        solde = Solde()
-        df: DataFrame = solde.prepare_data()
-        print('------------------------------------')
-        df.info()
-        print('------------------------------------')
-        # print(df.describe(include='all'))
-        # print('------------------------------------')
-        # print(df.shape)
+        solde: Solde = Solde()
+
     except Exception as ex:
         log.error(ex)
         log.error(traceback.format_exc())
